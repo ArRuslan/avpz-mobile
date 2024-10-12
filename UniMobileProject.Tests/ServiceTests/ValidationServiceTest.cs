782b0ba4c0ca8a69ed8144264d3d515dec36ba81
Kirikosha <kirikoshamain@gmail.com>
2024-10-12 20:13:56 +0200

    FEAT: Added unit tests and one test for validation

---
+++ UniMobileProject.Tests/ServiceTests/ValidationServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniMobileProject.src.Services.Validation;
+
+namespace UniMobileProject.Tests.ServiceTests
+{
+    public class ValidationServiceTest
+    {
+        private ValidationService _service;
+        public ValidationServiceTest()
+        {
+            _service = new ValidationService();
+        }
+
+        [Fact]
+        public void EmailValidation_NullInput_False()
+        {
+            //Arrange
+            string email = null;
+
+            //Action
+            var response = _service.EmailValidation(email);
+
+            //Assert
+            Assert.False(response.Item1);
+        }
+    }
+}
